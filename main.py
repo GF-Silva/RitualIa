23824c430f30d6eda005484a64fc2353ffccfa4a
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-03-24 20:55:59 -0300

    Adicionei o input de musicas e modifiquei o iframe

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from flask import Flask, render_template, request, jsonify
 # Create an instance of the Flask class
 app = Flask(__name__)
 
-music_videos = {
+# TODO: Calcular a duracao correta
+music_data = {
     "Cálice": {
         'artist': 'Chico Buarque',
-        'id': '9y2xB90A0CY',
-        'duration': '6000'
+        'music_id': '9y2xB90A0CY'
+    },
+    "Onde anda você": {
+        'artist': 'Vinicius de Moraes & Toquinho',
+        'music_id': 'Gb5sbORA62w'
     }
 }
 
@@ -16,17 +20,23 @@ music_videos = {
 def index():
     return render_template('index.html')
 
-@app.route("/get_video", methods=['POST'])
-def get_video():
-    # Process the POST request and return the video data
-    data = request.get_json()
-    video_name = data.get('video_name')
+@app.route("/get-music-id", methods=['GET'])
+def get_music_id():
+    
+    music_name = request.args.get("music_name")
+    
+    if music_name in music_data:
+        return jsonify({
+            'music_id': music_data[music_name]['music_id']
+        })
 
-    if video_name in music_videos:
-        video_id = music_videos[video_name]['id']
-        return jsonify({'video_id': video_id})
-    else:
-        return jsonify({ 'error': 'Video not found' }), 404
+    response = jsonify({
+        'error': 'music not found'
+    }), 404
+
+    return response
+
+
 
 # Optional: Run the application directly when the script is executed
 if __name__ == '__main__':

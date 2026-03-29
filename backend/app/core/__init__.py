7c9568878ec5b017b1ba9eb3506296ee0d9b9510
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-03-29 16:32:58 -0300

    Remove: Tirei uma classe não utilizada

--- backend/app/core/__init__.py
+++ backend/app/core/__init__.py
@@ -1,8 +1,4 @@
 from .database import Database
-from .local_database import LocalDatabase
-from .music_cache import MusicCache
 from .config import DB_CONFIG
 
 database = Database(**DB_CONFIG)
-local_database = LocalDatabase(database)
-music_cache = MusicCache()

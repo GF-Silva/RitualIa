5346dc1e70aaf64f7bce65646264a8ed6a85ceeb
GF-Silva <gabriel.ferreira.profile@gmail.com>
2026-04-07 15:07:31 -0300

    Rename get_song_played_count to get_song_play_count

--- backend/app/api/__init__.py
+++ backend/app/api/__init__.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter
-from .routes import get_song_played_count, get_songs_by_filter, on_song_play
+from .routes import get_song_play_count, get_songs_by_filter, on_song_play
 
 from .routes import testes
 
@@ -8,4 +8,4 @@ api_router = APIRouter()
 api_router.include_router(testes.router)
 api_router.include_router(get_songs_by_filter.router)
 api_router.include_router(on_song_play.router)
-api_router.include_router(get_song_played_count.router)
+api_router.include_router(get_song_play_count.router)

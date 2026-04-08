f0522b62ea1d1a44dd48d7fbfe6160dd42699963
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-04-07 23:29:22 -0300

    chore: corrige gitignore para ignorar caches e arquivos locais

--- backend/app/api/routes/get_songs_by_filter.py
+++ backend/app/api/routes/get_songs_by_filter.py
@@ -5,5 +5,5 @@ from app.core import database
 router = APIRouter()
 
 @router.get('/get-songs-by-filter', summary="Obtem todas as musicas que tenham os dados fornecidos")
-def get_songs_by_filter(genre: str | None = None, emotions: str | None = None, limit: int = 10):
+def get_songs_by_filter(genre: str | None = None, emotions: str | None = None, limit: int = 1):
     return database.get_songs_by_filter(genre, emotions, limit)

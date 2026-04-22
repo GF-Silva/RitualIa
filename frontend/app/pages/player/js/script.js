3fc11bb5b63b32e0b582f2478b9abfe9ac183ef1
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-04-22 00:19:43 -0300

    comecei a integrar o novo frontend

--- frontend/app/pages/player/js/script.js
+++ frontend/app/pages/player/js/script.js
@@ -1,13 +1,44 @@
 function togglePanel() {
-    const panel = document.getElementById("panel");
-    const playerBox = document.getElementById("playerBox");
+  document.getElementById("panel").classList.toggle("open");
+  document.getElementById("playerBox").classList.toggle("shift");
+}
 
-    panel.classList.toggle("open");
-    playerBox.classList.toggle("shift");
+function toggleQueue() {
+  document.getElementById("queue").classList.toggle("open");
+}
+
+/* YOUTUBE */
+let player;
+
+function onYouTubeIframeAPIReady() {
+  player = new YT.Player('ytplayer', {
+    videoId: 'syqJAgTQdlU',
+    playerVars: { autoplay: 1, controls: 0 },
+    events: {
+      onReady: (e) => {
+        e.target.mute();
+        e.target.playVideo();
+
+        setInterval(() => {
+          const d = player.getDuration();
+          const c = player.getCurrentTime();
+
+          if (d) {
+            document.getElementById("duration").innerText = format(d);
+            document.getElementById("current").innerText = format(c);
+
+            const p = (c / d) * 100;
+            document.getElementById("bar").style.width = p + "%";
+            document.getElementById("dot").style.left = p + "%";
+          }
+        }, 500);
+      }
+    }
+  });
 }
 
 function format(t) {
-    const m = Math.floor(t / 60);
-    const s = Math.floor(t % 60);
-    return m + ":" + (s < 10 ? "0" : "") + s;
+  const m = Math.floor(t / 60);
+  const s = Math.floor(t % 60);
+  return m + ":" + (s < 10 ? "0" : "") + s;
 }

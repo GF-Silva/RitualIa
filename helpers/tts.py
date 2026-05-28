a24a8ecd2421b0ca3f37d0e9fd5d148ff0861bdc
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-05-28 20:33:46 -0300

    fix: tts helper funciona com input agora

--- helpers/tts.py
+++ helpers/tts.py
@@ -11,14 +11,12 @@ speech_config = speechsdk.SpeechConfig(subscription=os.getenv("AZURE_SPEECH_API_
 # Lista de vozes: https://aka.ms/csspeech/voicenames
 speech_config.speech_synthesis_voice_name = "pt-BR-Thalita:DragonHDLatestNeural"
 
-audio_config = speechsdk.audio.AudioOutputConfig(filename="saida.wav")
+nome = input("Digite o nome da música: ")
+audio_config = speechsdk.audio.AudioOutputConfig(filename=f"{nome}.wav")
 # 3. Cria o sintetizador (usando a saída de som padrão)
 sintetizador = speechsdk.SpeechSynthesizer(speech_config=speech_config, audio_config=audio_config)
 
-
-texto = """
-Serj Tankian, do System of a Down, nasceu no Líbano e revolucionou o metal alternativo. O hit Toxicity, lançado em 2001, captura o caos urbano e a alienação social daquela era. Musicalmente, a faixa brilha pela alternância entre versos melódicos e refrões explosivos, marcados por guitarras pesadas e batidas sincopadas. Ao abordar a toxicidade das relações humanas, a música tornou-se um hino de angústia e resistência para toda uma geração. Com indicações ao Grammy, o tema consolidou seu legado como um pilar essencial e atemporal do rock moderno.
-"""
+texto = input("Digite o conteudo do texto: ")
 
 # 4. Executa a síntese de forma síncrona
 sintetizador.speak_text_async(texto).get()

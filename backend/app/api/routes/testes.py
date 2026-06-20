a13fe948b6198faebe4b41e423ec8a2091b0e8c3
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-06-20 19:23:10 -0300

    troca o fastapi para flask

--- backend/app/api/routes/testes.py
+++
@@ -1,8 +0,0 @@
-from fastapi import APIRouter
-from app.core import database
-
-router = APIRouter()
-
-@router.get('/teste', summary="Rota para testes")
-def call_tst():
-    return
--- backend/app/core/__init__.py
+++
@@ -1,4 +0,0 @@
-from .database import Database
-from .config import DB_CONFIG
-
-database = Database(**DB_CONFIG)

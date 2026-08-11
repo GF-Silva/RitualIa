c308c132622e5973fd046fa89f0b5d451dde707b
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-08-10 23:56:49 -0300

    feat (backend, main): Adiciona um error handler HTTPException

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+from werkzeug.exceptions import HTTPException
 
 from routes.songs import songs_bp
 from routes.copa import copa_bp
@@ -7,6 +8,13 @@ app = Flask(__name__,
             template_folder='pages',
             static_folder='pages',
             static_url_path='/pages')
+
+@app.errorhandler(HTTPException)
+def handle_http_error(error):
+    return jsonify({
+        'error': f'{error.name}',
+        'detail': error.description
+    }), error.code
 
 app.register_blueprint(songs_bp)
 app.register_blueprint(copa_bp)

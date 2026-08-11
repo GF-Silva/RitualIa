e060a75b6101d9cfb2926ac3c8b34cc328b9b5ca
GS-Silva <gabriel.ferreira.profile@gmail.com>
2026-08-10 23:35:30 -0300

    Altera o songs para consumir o dict do cursor

--- app/routes/songs.py
+++ app/routes/songs.py
@@ -4,7 +4,7 @@ from core import database
 
 songs_bp = Blueprint('songs', __name__, url_prefix="/songs")
 
-@songs_bp.route('/', methods=['GET'])
+@songs_bp.route('', methods=['GET'])
 def get_songs():
     genre = request.args.get('genre')
     emotion = request.args.get('emotion')
@@ -21,11 +21,11 @@ def get_songs():
     if not emotion_result and emotion is not None:
         abort(404, description=f"Emoção '{emotion}' não encontrada.")
 
-    genre_id = genre_result[0][0] if genre_result else None
-    emotion_id = emotion_result[0][0] if emotion_result else None
+    genre_id = genre_result[0]['id'] if genre_result else None
+    emotion_id = emotion_result[0]['id'] if emotion_result else None
 
     music = database.get_songs_by_filter(genre_id, emotion_id, limit)
 
     if not music: abort(404, description ="Nenhuma música encontrada pra essa combinação")
     
-    return music
+    return music    
